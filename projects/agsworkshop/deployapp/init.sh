14a07a1646b88290fe583b64fff4d92b64597d42
liangfb <liang.fb@gmail.com>
2021-03-01 21:10:23 +0800

    Update init.sh

--- projects/agsworkshop/deployapp/init.sh
+++ projects/agsworkshop/deployapp/init.sh
@@ -8,6 +8,12 @@ sed -i "s/%host%/$1/g" /var/www/html/app/etc/env.php
 sed -i "s/%username%/$2/g" /var/www/html/app/etc/env.php
 sed -i "s/%password%/$3/g" /var/www/html/app/etc/env.php
 
+mysql -h$1 -u$2 -p$3 -e "drop database magento;"
+
 mysql -h$1 -u$2 -p$3 < magento.sql
 
-update magento.core_config_data set web/unsecure/base_url='http://$4/', web/secure/base_url='http://$4/', web/unsecure/base_url='http://$4/' where path like '%base_url';
+mysql -h$1 -u$2 -p$3 -e "update magento.core_config_data set web/unsecure/base_url='http://$4/', web/secure/base_url='http://$4/', web/unsecure/base_url='http://$4/' where path like '%base_url';"
+
+/var/www/html/bin/magento cache:clean
+/var/www/html/bin/magento setup:di:compile
+/var/www/html/bin/magento setup:static-content:Deploy -f
