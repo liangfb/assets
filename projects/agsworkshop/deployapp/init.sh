5b330d3242d17b6156a22ad40717ed727e746f29
liangfb <liang.fb@gmail.com>
2021-03-01 21:30:59 +0800

    Update init.sh

--- projects/agsworkshop/deployapp/init.sh
+++ projects/agsworkshop/deployapp/init.sh
@@ -12,7 +12,7 @@ mysql -h$1 -u$2 -p$3 -e "drop database magento;"
 
 mysql -h$1 -u$2 -p$3 < magento.sql
 
-mysql -h$1 -u$2 -p$3 -e "update magento.core_config_data set web/unsecure/base_url='http://$4/', web/secure/base_url='http://$4/', web/unsecure/base_url='http://$4/' where path like '%base_url';"
+mysql -h$1 -u$2 -p$3 -e "update magento.core_config_data set value='http://$4/' where path like '%base_url';"
 
 /var/www/html/bin/magento cache:clean
 /var/www/html/bin/magento setup:di:compile
