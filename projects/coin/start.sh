6be827acdbdfddfe8c22c78a0c675646a6cc16d2
liangfb <liang.fb@gmail.com>
2021-05-15 22:02:38 +0800

    Update start.sh

--- projects/coin/start.sh
+++ projects/coin/start.sh
@@ -5,7 +5,7 @@ chia start farmer
 instanceid=$(curl -s 169.254.169.254/1.0/meta-data/instance-id)
 echo "plot" >> $instanceid.log
 echo $(date) >> $instanceid.log
-echo "chia plots create ..." >> test.txt
+chia plots create -k 32 -r 2 -n 1 -t /mnt/tmp -d /chia
 echo "plot completed" >> $instanceid.log
 echo $(ll /chia/) >> $instanceid.log
 echo "copy" >> $instanceid.log
