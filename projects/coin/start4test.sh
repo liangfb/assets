dc48d77488f86aecef14bafedba66964e2f595a0
liangfb <liang.fb@gmail.com>
2021-05-22 15:08:24 +0800

    Update start4test.sh

--- projects/coin/start4test.sh
+++ projects/coin/start4test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-echo "bucket:"$1 >> $instanceid.log
+sudo su
+echo $1 >> $instanceid.log
 sudo yum update -y
 #sudo yum install python3 git -y
 #git clone https://github.com/Chia-Network/chia-blockchain.git -b latest
@@ -13,16 +14,14 @@ echo $(date) >> $instanceid.log
 echo "plot completed" >> $instanceid.log
 echo $(ll /mnt/data/testdir/) >> $instanceid.log
 echo "copy" >> $instanceid.log
-
 echo "testcontent" >> /mnt/data/testdir/asdfghjhkjl.plot
-
 whilecondition=true
 while [ $whilecondition=true ]
 do
     aws s3 sync --storage-class ONEZONE_IA /mnt/data/testdir/ s3://$1
     if [ "$?" -eq 0 ]
     then
-        echo "1" >> $instanceid.log
+        echo "inif" >> $instanceid.log
         filename=$(ls /mnt/data/testdir/*.plot)
         echo $filename >> $instanceid.log
         totalFoundObject=$(aws s3 ls s3://${1}/${filename} --recursive --summarize | grep "Total Objects: " | sed 's/[^0-9]*//g')
